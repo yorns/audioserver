407176b9cdd951cf7fa792142f4b4739fe5c7100
Yorn <yorn@gmx.net>
2020-03-20 09:51:47 +0100

    copy all necessary files to install directories

--- cmake/createdirs.cmake
+++ cmake/createdirs.cmake
@@ -5,6 +5,7 @@ set( AUDIO_MP3_DIR ${AUDIO_DATA_DIR}/mp3 )
 set( AUDIO_TMP_DIR ${AUDIO_DATA_DIR}/tmp )
 set( AUDIO_HTML_DIR ${AUDIO_DATA_DIR}/html )
 set( AUDIO_HTMLIMG_DIR ${AUDIO_DATA_DIR}/html/img )
+set( AUDIO_HTMLFONT_DIR ${AUDIO_DATA_DIR}/html/font )
 set( AUDIO_HTMLJS_DIR ${AUDIO_DATA_DIR}/html/js )
 set( AUDIO_HTMLCSS_DIR ${AUDIO_DATA_DIR}/html/css )
 set( AUDIO_PLAYLIST_DIR ${AUDIO_DATA_DIR}/playlist )
@@ -27,6 +28,10 @@ install ( DIRECTORY DESTINATION ${AUDIO_HTMLIMG_DIR} DIRECTORY_PERMISSIONS
           GROUP_WRITE GROUP_READ GROUP_EXECUTE
           WORLD_WRITE WORLD_READ WORLD_EXECUTE )
 install ( DIRECTORY DESTINATION ${AUDIO_HTMLJS_DIR} DIRECTORY_PERMISSIONS
+       OWNER_WRITE OWNER_READ OWNER_EXECUTE
+        GROUP_WRITE GROUP_READ GROUP_EXECUTE
+        WORLD_WRITE WORLD_READ WORLD_EXECUTE )
+install ( DIRECTORY DESTINATION ${AUDIO_HTMLFONT_DIR} DIRECTORY_PERMISSIONS
        OWNER_WRITE OWNER_READ OWNER_EXECUTE
         GROUP_WRITE GROUP_READ GROUP_EXECUTE
         WORLD_WRITE WORLD_READ WORLD_EXECUTE )
@@ -54,11 +59,24 @@ install ( FILES
        ${CMAKE_SOURCE_DIR}/html/js/bootstrap.min.js
        ${CMAKE_SOURCE_DIR}/html/js/jquery-3.4.1.min.js
        ${CMAKE_SOURCE_DIR}/html/js/popper.min.js
+       ${CMAKE_SOURCE_DIR}/html/js/audioserver.js
       DESTINATION ${AUDIO_HTMLJS_DIR}
       PERMISSIONS
       OWNER_READ OWNER_WRITE
       GROUP_READ
       WORLD_READ )
+
+  install ( FILES
+         ${CMAKE_SOURCE_DIR}/html/font/fontello.svg
+         ${CMAKE_SOURCE_DIR}/html/font/fontfontello.ttf
+         ${CMAKE_SOURCE_DIR}/html/font/fontfontello.woff
+         ${CMAKE_SOURCE_DIR}/html/font/fontfontello.woff2
+        DESTINATION ${AUDIO_HTMLFONT_DIR}
+        PERMISSIONS
+        OWNER_READ OWNER_WRITE
+        GROUP_READ
+        WORLD_READ )
+
 
 install ( FILES
        ${CMAKE_SOURCE_DIR}/html/css/bootstrap.min.css
