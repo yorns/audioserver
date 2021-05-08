bc8a26c047a4421c9deef4d676a43cf889466d5a
Joerg-Christian Boehme <joerg@chaosdorf.de>
2021-05-08 04:22:50 +0200

    Fix compile issue

--- test/PlayerCmdl.cpp
+++ test/PlayerCmdl.cpp
@@ -64,7 +64,7 @@ private:
             break;
         }
         case 'c': {
-            m_player->toogleLoop();
+            m_player->toggleLoop();
             break;
         }
         case 'x': {
